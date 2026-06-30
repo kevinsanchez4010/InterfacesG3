99ea32e0ff6d40e8e6372e76bce2d15b0b537225
shaulinumajinga3559-collab <shaulin.umajinga3559@utc.edu.ec>
2026-06-30 15:13:39 -0500

    3 Nombre-proveedor

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_proveedores, Nombre='listar_proveedores'),
-    path('crear_proveedor/', views.crear_proveedor, name='crear_proveedor'),
+    path('crear_proveedor/', views.crear_proveedor, Nombre='crear_proveedor'),
     path('eliminar_proveedor/<int:id>/', views.eliminar_proveedor, name='eliminar_proveedor'),
     path('editar_proveedor/<int:id>/', views.editar_proveedor, name='editar_proveedor'),
 ]
