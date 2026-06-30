06dcc6aefa9962b3c122a731e53ea1b7772fff26
shaulinumajinga3559-collab <shaulin.umajinga3559@utc.edu.ec>
2026-06-30 15:07:38 -0500

    1 Nombre

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [ 
+    path('', views.listar_proveedores, Nombre='listar_proveedores'),
+    path('agregar/', views.agregar_proveedor, name='agregar_proveedor'),
+    path('editar/<int:id>/', views.editar_proveedor, name='editar_proveedor'),
+    path('eliminar/<int:id>/', views.eliminar_proveedor, name='eliminar_proveedor'),
+]
