cd9ea8c79048b992c3b7d6f974d4134ef169fe44
shaulinumajinga3559-collab <shaulin.umajinga3559@utc.edu.ec>
2026-06-30 15:18:09 -0500

    5 Nombre-editar-provee

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.listar_proveedores, Nombre='listar_proveedores'),
     path('crear_proveedor/', views.crear_proveedor, Nombre='crear_proveedor'),
     path('eliminar_proveedor/<int:id>/', views.eliminar_proveedor, Nombre='eliminar_proveedor'),
-    path('editar_proveedor/<int:id>/', views.editar_proveedor, name='editar_proveedor'),
+    path('editar_proveedor/<int:id>/', views.editar_proveedor, Nombre='editar_proveedor'),
 ]
