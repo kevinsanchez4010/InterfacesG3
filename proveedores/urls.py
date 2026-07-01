4dda9051ba45d0fc89ac11e2b79af3ff9644226e
kevinsanchez4010 <kevin.sanchez4010@utc.edu.ec>
2026-07-01 12:06:59 -0500

    correccion de errores

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listar_proveedores, Nombre='listar_proveedores'),
-    path('crear_proveedor/', views.crear_proveedor, Nombre='crear_proveedor'),
-    path('eliminar_proveedor/<int:id>/', views.eliminar_proveedor, Nombre='eliminar_proveedor'),
-    path('editar_proveedor/<int:id>/', views.editar_proveedor, Nombre='editar_proveedor'),
+    path('', views.listar_proveedores, name='listar_proveedores'),
+    path('crear_proveedor/', views.crear_proveedor, name='crear_proveedor'),
+    path('eliminar_proveedor/<int:id>/', views.eliminar_proveedor, name='eliminar_proveedor'),
+    path('editar_proveedor/<int:id>/', views.editar_proveedor, name='editar_proveedor'),
 ]
